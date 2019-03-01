b74116bbc1781f10e24ba32cefc4ce75e5519ce2
mvsayewich <mvsayewich@gmail.com>
2019-02-28 21:10:53 -0500

    Mv Push

--- static/js/HPI.js
+++ static/js/HPI.js
@@ -3,9 +3,10 @@
 const ENABLED_OPACITY = 1;
 const DISABLED_OPACITY = .2;
 
-d3.csv('https://github.com/mvsayewich/project3/blob/master/datasets/mergeddataset.csv')
+d3.csv('https://raw.githubusercontent.com/mvsayewich/project3/master/datasets/mergeddataset.csv')
   .then(data => draw(data));
 
+console.log('test')
 function draw(data) {
   const margin = {top: 20, right: 20, bottom: 50, left: 50};
   const width = 750 - margin.left - margin.right;
