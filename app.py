2b0967c3d75703dafd48d45c54d04f3c967a5b7a
WS <43322228+johnshao89@users.noreply.github.com>
2019-03-09 18:11:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 #import modules
-from flask import Flask, render_template,request,jsonify
-from flask_sqlalchemy import SQLAlchemy
-import os
+#import modules
+from flask import Flask, render_template,jsonify
+#from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
-import numpy as np
 import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from collections import defaultdict
 
