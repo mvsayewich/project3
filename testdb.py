46f05045611d4626a5c59ae6a9f097922d886334
WS <43322228+johnshao89@users.noreply.github.com>
2019-03-09 17:50:28 -0500

    Delete testdb.py

--- testdb.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect("//User//yinyue//Documents//GitHub//project3//db//output.db")
-cursor = connection.cursor()
-cursor.execute("SELECT * from Real_Estate") 
